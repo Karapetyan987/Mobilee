ffb4d6fa3623e1bfd29d9c1cac4695200178c9a8
karapetyan987 <narekkarapetyan2015@mail.ru>
2020-09-03 18:11:38 +0400

    Добавьте файлы проекта.

---
+++ Mobile/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Mobile
+{
+
+    public enum Memory
+    {
+        less = 16,
+        mid = 32,
+        more = 64
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Mobile> Mobiles = new List<Mobile>()
+            {
+            new Iphone9(ConsoleColor.DarkGreen),
+            new SamsungA10(ConsoleColor.DarkYellow,Memory.more),
+            new NokiaX6()
+            };
+            foreach (var item in Mobiles)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+            List<Mobile> Brends = new List<Mobile>()
+            {
+               new Apple(),
+               new Samsung(),
+               new Nokia()
+            };
+            foreach (var item in Brends)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Mobile/Samsung.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mobile
+{
+    public class Samsung : Mobile
+    {
+        public override string BrendsName { get => "Samsung"; }
+        public override string OgtagortsoxeriQanak { get => "295 Mln Users"; }
+        public Samsung()
+        {
+
+        }
+        public override string ToString()
+        {
+            return BrendsName + " - " + OgtagortsoxeriQanak;
+
+        }
+    }
+}
